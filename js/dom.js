06cdaac1b96c74cdffef54667bda48d640529ac2
soy <roki5220@naver.com>
2020-10-26 17:51:04 +0900

    1026수업추가

---
+++ js/dom.js
@@ -0,0 +1,5 @@
+console.log(window);
+// let text = this.document.children[1].children[1];
+console.log(this.document.childNodes[1].children[1].children[0].innerHTML = 'hello');
+console.log(this.document.childNodes[1].children[1].childNodes);
+console.log(this.document.childNodes[1].children[1].childNodes[14].nodeValue = 'days:');
